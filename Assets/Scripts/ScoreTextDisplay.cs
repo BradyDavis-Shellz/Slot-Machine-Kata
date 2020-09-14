fd8dd7a970666901bc7affd96a23806b475fbd18
BradyDavis-Shellz <bradydavis.ver.2.0@gmail.com>
2020-09-14 17:17:26 -0500

    Adding missing state, lerping score text display up on win

--- Assets/Scripts/ScoreTextDisplay.cs
+++ Assets/Scripts/ScoreTextDisplay.cs
@@ -1,17 +1,44 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreTextDisplay : MonoBehaviour
 {
-    private int score = 0;
+    private int score;
+
+    private float timer = 0f;
 
     [SerializeField]
     private Text scoreText;
 
+    [SerializeField] 
+    private float duration = 1.5f;
+
     private void setScoreText()
     {
         scoreText.text = String.Format("Score: {0}", score);
+    }
+
+    private IEnumerator UpdateScore(int target)
+    {
+        int start = score;
+        for (float timer = 0; timer < duration; timer += Time.deltaTime)
+        {
+            float progress = timer / duration;
+
+            score = (int) Mathf.Lerp(start, target, progress);
+            setScoreText();
+            yield return null;
+        }
+
+        score = target;
+        setScoreText();
+    }
+    
+    public void AddPoints(int points)
+    {
+        StartCoroutine(UpdateScore(score + points));
     }
     
     void Start()
@@ -25,12 +52,6 @@ public class ScoreTextDisplay : MonoBehaviour
         {
             Debug.LogError(String.Format("No Text component found on {0}", gameObject.name));
         }
-        setScoreText();
-    }
-
-    public void AddPoints(int points)
-    {
-        score += points;
         setScoreText();
     }
 }
